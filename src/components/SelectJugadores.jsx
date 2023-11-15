56afd1ec4304efacd5fe901dec7605aa6acd7c94
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-11-15 18:14:50 -0300

    estilos admin page

---
+++ src/components/SelectJugadores.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import './SelectJugadores.css';
+
+export const SelectJugadores = ({ jugadores }) => {
+    const [equipoBlanco, setEquipoBlanco] = useState([]);
+    const [equipoNegro, setEquipoNegro] = useState([]);
+  
+    const handleSelectChange = (event, equipo) => {
+      const selectedPlayer = event.target.value;
+      if (selectedPlayer) {
+        if (equipo === 'blanco' && equipoBlanco.length < 7) {
+          setEquipoBlanco(equipoBlanco.concat(selectedPlayer));
+        } else if (equipo === 'negro' && equipoNegro.length < 7) {
+          setEquipoNegro(equipoNegro.concat(selectedPlayer));
+        }
+      }
+    };
+  
+    const quitarJugador = (jugador, equipo) => {
+      if (equipo === 'blanco') {
+        setEquipoBlanco(equipoBlanco.filter(nombre => nombre !== jugador));
+      } else if (equipo === 'negro') {
+        setEquipoNegro(equipoNegro.filter(nombre => nombre !== jugador));
+      }
+    };
+  
+    // Para evitar seleccionar el mismo jugador en ambos equipos,
+    // filtramos los jugadores que ya están seleccionados en alguno de los equipos.
+    const jugadoresDisponibles = jugadores.filter(jugador =>
+      !equipoBlanco.includes(jugador.nombre) && !equipoNegro.includes(jugador.nombre)
+    );
+  
+    return (
+      <div className='container'>
+        <h2>Forma tus Equipos</h2>
+        <div>
+          <label className='team-section' htmlFor="selectBlanco">Equipo Blanco:</label>
+          <select className='team-section.team-blanco' id="selectBlanco" onChange={(e) => handleSelectChange(e, 'blanco')} defaultValue="">
+            <option disabled value="">Selecciona un jugador</option>
+            {jugadoresDisponibles.map(jugador => (
+              <option key={jugador.id} value={jugador.nombre}>{jugador.nombre}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {equipoBlanco.map(jugador => (
+            <div key={jugador}>
+              {jugador}
+              <button  className='remove-button ' onClick={() => quitarJugador(jugador, 'blanco')}>Quitar</button>
+            </div>
+          ))}
+        </div>
+        <div>
+          <label className='team-section' htmlFor="selectNegro">Equipo Negro:</label>
+          <select className='team-section.team-negro' id="selectNegro" onChange={(e) => handleSelectChange(e, 'negro')} defaultValue="">
+            <option disabled value="">Selecciona un jugador</option>
+            {jugadoresDisponibles.map(jugador => (
+              <option key={jugador.id} value={jugador.nombre}>{jugador.nombre}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {equipoNegro.map(jugador => (
+            <div key={jugador}>
+              {jugador}
+              <button className='remove-button' onClick={() => quitarJugador(jugador, 'negro')}>Quitar</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
--- src/pages/AdminScreen.jsx
+++ src/pages/AdminScreen.jsx
@@ -1,56 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { SelectJugadores } from '../components/SelectJugadores';
 
-export const AdminScreen = ({ jugadores = [] }) => {
-  
-  console.log("AdminScreen jugadores:", jugadores);
-  const [equipoBlanco, setEquipoBlanco] = useState([]);
-  const [equipoNegro, setEquipoNegro] = useState([]);
+export const AdminScreen = () => {
+  const [jugadores, setJugadores] = useState([]);
 
-  console.log(jugadores);
-  const handleSelectChange = (event, equipo) => {
-    const selectedPlayers = Array.from(event.target.selectedOptions, option => option.value);
-    if (equipo === 'blanco') {
-      setEquipoBlanco(selectedPlayers);
-    } else {
-      setEquipoNegro(selectedPlayers);
-    }
-  };
+  useEffect(() => {
+    fetch("/jugadores.json")
+      .then((response) => response.json())
+
+      .then((data) => setJugadores(data))
+
+      .catch((error) => console.error("Error al cargar los jugadores:", error));
+  }, []);
+ 
   return (
-    <div>
-      <h2>Forma tus Equipos</h2>
-      <div>
-        <label htmlFor="equipoBlanco">Equipo Blanco:</label>
-        <select multiple id="equipoBlanco" value={equipoBlanco} onChange={(e) => handleSelectChange(e, 'blanco')}>
-          {jugadores.map(jugador => (
-            <option key={jugador.id} value={jugador.nombre}>{jugador.nombre}</option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="equipoNegro">Equipo Negro:</label>
-        <select multiple id="equipoNegro" value={equipoNegro} onChange={(e) => handleSelectChange(e, 'negro')}>
-          {jugadores.map(jugador => (
-            <option key={jugador.id} value={jugador.nombre}>{jugador.nombre}</option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <h3>Selecciones del Equipo Blanco:</h3>
-        <ul>
-          {equipoBlanco.map(nombreJugador => (
-            <li key={nombreJugador}>{nombreJugador}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h3>Selecciones del Equipo Negro:</h3>
-        <ul>
-          {equipoNegro.map(nombreJugador => (
-            <li key={nombreJugador}>{nombreJugador}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <SelectJugadores jugadores={jugadores}/>
   );
 };
