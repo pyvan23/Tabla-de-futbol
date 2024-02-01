8f91b9760dcebca8816f0d29a5077973f0b7a85d
pyvan23 <ivanmiguelalvarez@gmail.com>
2024-02-01 19:09:59 -0300

    captura de imagen

--- src/pages/AdminScreen.jsx
+++ src/pages/AdminScreen.jsx
@@ -1,10 +1,34 @@
 /* eslint-disable react/prop-types */
 import { SelectJugadores } from "../components/SelectJugadores";
+import html2canvas from 'html2canvas';
 
 export const AdminScreen = () => {
+
+  const captureScreen = () => {
+    html2canvas(document.body).then((canvas) => {
+      let imgData = canvas.toDataURL('image/png');
+      downloadImage(imgData);
+    });
+  }
+
+  const downloadImage = (data) => {
+    let link = document.createElement('a');
+    link.href = data;
+    link.download = 'screenshot.png';
+    link.click();
+  }
+
+
+
+
+
+  
   return (
     <>
       <SelectJugadores />
+      <button onClick={captureScreen}>
+        Capturar pantalla
+      </button>
     </>
   );
 };
